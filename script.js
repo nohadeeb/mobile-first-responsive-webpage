b2c25deba26fc53959bfbb1dc94df9b94b495d8b
nohadeeb <noha.el.deeb2002@gmail.com>
2021-01-24 17:37:56 +0200

    mobile view finished

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
   this.classList.toggle("active");
   var dropdownContent = this.nextElementSibling;
+  
   if (dropdownContent.style.display === "block") {
   dropdownContent.style.display = "none";
   } else {
